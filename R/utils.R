b7abc736ce9df22211adebf8dcce381c8fd6fe69
Gustavo Velásquez <gvelasq2github@gmail.com>
2018-05-28 13:22:19 -0400

    add key and color utils

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,40 @@
 #' @keywords internal
 #' @noRd
-#' @importFrom dplyr mutate
-#' @importFrom tibble as_tibble
 
 token_env <- new.env(parent = emptyenv())
 
-get_token <- function() {
+get_local_token <- function() {
   get0("token", envir = token_env)
 }
 
-set_token <- function(value) {
+set_local_token <- function(value) {
   token_env$token <- value
 }
 
-reset_token <- function() {
+reset_local_token <- function() {
   suppressWarnings(rm("token", envir = token_env))
+}
+
+get_local_key <- function() {
+  get0("key", envir = token_env)
+}
+
+set_local_key <- function(value) {
+  token_env$key <- value
+}
+
+reset_local_key <- function() {
+  suppressWarnings(rm("key", envir = token_env))
+}
+
+color_blue <- function(x) {
+  crayon::blue(x)
+}
+
+color_grey <- function(x) {
+  crayon::make_style("darkgrey")(x)
+}
+
+color_red <- function(x) {
+  crayon::red(x)
 }
