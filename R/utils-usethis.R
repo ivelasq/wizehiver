7e8083b872ecd30a0b4f6725eb0b35788e949125
Gustavo Velásquez <gvelasq2github@gmail.com>
2018-05-28 13:24:07 -0400

    export edit_r_environ() from usethis

---
+++ R/utils-usethis.R
@@ -0,0 +1,15 @@
+#' Edit .Renviron
+#'
+#' See \code{usethis::\link[usethis]{edit_r_environ}} for details.
+#'
+#' This function opens \code{.Renviron} where you can store your Zengine™ API
+#' key and personal access token (PAT). It is called by \code{set_key()} and \code{set_token()}.
+#'
+#' @name usethis-helper
+#' @rdname usethis-helper
+#' @keywords internal
+#' @aliases            edit_r_environ
+#' @importFrom usethis edit_r_environ
+#' @export             edit_r_environ
+#' @usage edit_r_environ()
+NULL
