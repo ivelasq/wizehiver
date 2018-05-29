df4efde13f214f9138beaa34b4a53e7db87b723e
Gustavo Velásquez <gvelasq2github@gmail.com>
2018-05-28 20:22:19 -0400

    remove \u2122

--- R/auth.R
+++ R/auth.R
@@ -1,11 +1,11 @@
-#' Manage Zengine™ API key and PAT
+#' Manage Zengine API key and PAT
 #'
 #' @name auth-helpers
 #' @aliases get_token set_token get_key set_key
 #' @export get_token set_token get_key set_key
 #'
 #' @description
-#' These functions retrieve and store your Zengine™ API key and personal access token (PAT) in \code{.Renviron}, with help from \code{usethis::edit_r_environ()}.
+#' These functions manage your Zengine API key and personal access token (PAT) in \code{.Renviron}.
 #' @usage
 #' get_key()
 #' set_key()
@@ -13,7 +13,7 @@
 #' set_token()
 #'
 #' @return
-#' \code{get_key()} and \code{get_token()} return a character vector with the Zengine™ API key and PAT stored in \code{.Renviron}, respectively. If these values are not stored in \code{.Renviron}, the functions return \code{NULL}.
+#' \code{get_key()} and \code{get_token()} return a character vector with the Zengine API key and PAT stored in \code{.Renviron}, respectively. If these values are not stored in \code{.Renviron}, the functions return \code{NULL}.
 #'
 #' \code{set_key()} and \code{set_token()} invisibly return \code{NULL}.
 get_token <- function() {
@@ -39,7 +39,7 @@ set_token <- function() {
   cli::cat_bullet(paste0("Calling ", color_grey("`usethis::edit_r_environ()`"), " to open ", color_blue("'.Renviron'")), bullet = "tick", bullet_col = "green")
   usethis::edit_r_environ()
   cli::cat_bullet(paste0(color_blue("'.Renviron'"), "must end with a newline"), bullet_col = "red")
-  cli::cat_bullet("Store your Zengine™ personal access token (PAT), see example below:", bullet_col = "red")
+  cli::cat_bullet("Store your Zengine personal access token (PAT), see example below:", bullet_col = "red")
   message("ZENGINE_PAT=ABC123XYZ")
   invisible(NULL)
 }
@@ -67,7 +67,7 @@ set_key <- function() {
   cli::cat_bullet(paste0("Calling ", color_grey("`usethis::edit_r_environ()`"), " to open ", color_blue("'.Renviron'")), bullet = "tick", bullet_col = "green")
   usethis::edit_r_environ()
   cli::cat_bullet(paste0(color_blue("'.Renviron'"), "must end with a newline"), bullet_col = "red")
-  cli::cat_bullet("Store your Zengine™ API key, see example below:", bullet_col = "red")
+  cli::cat_bullet("Store your Zengine API key, see example below:", bullet_col = "red")
   message("ZENGINE_KEY=ABC123XYZ")
   invisible(NULL)
 }
